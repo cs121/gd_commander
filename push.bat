adeb204be47692faa8c90fe17510c5670855f384
christian schmidt <chris@skulltree.de>
2025-06-05 00:29:53 +0200

    commit - 2025-06-05

--- push.bat
+++ push.bat
@@ -1,5 +1,14 @@
 @echo off
+setlocal enabledelayedexpansion
+
+:: Datum im Format YYYY-MM-DD holen
+for /f %%a in ('wmic os get localdatetime ^| find "."') do set datetime=%%a
+set date=!datetime:~0,4!-!datetime:~4,2!-!datetime:~6,2!
+
+:: Commit-Nachricht abfragen
+set /p commitMessage=Gib die Commit-Nachricht ein: 
+
 echo PUSH
 git add .
-git commit -m "commit"
+git commit -m "%commitMessage% - %date%"
 git push origin main
