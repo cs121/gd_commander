de3a47b8ee1a2113372e49150a5bb30bdfe53e3b
christian schmidt <chris@skulltree.de>
2025-06-05 08:08:24 +0200

    commit - 2025-06-05

--- Levels/Level01/level1.gd
+++ Levels/Level01/level1.gd
@@ -9,13 +9,16 @@ func _ready():
 	# Seed global random number generator for replicable first level
 	seed(123)
 	# Instantiate orbs
+# Lade die Szene einmal außerhalb der Schleife (Performance!)
+	var dralthi_scene := preload("res://Scenes/Ships/Enemy/Dralthi.tscn")
+
 	for x in range(ORB_COUNT):
-		var orb = Orb.new_orb()
-		$RedTeam.add_child(orb)
-		$RedTeam.set_team_properties(orb)
-		orb.position_randomly()
-		# Connect the destroyed signal of the orb to the orb_died function
-		orb.destroyed.connect(orb_died)
+		var dralthi = dralthi_scene.instantiate()
+		$RedTeam.add_child(dralthi)
+		$RedTeam.set_team_properties(dralthi)
+		dralthi.position_randomly()
+		#dralthi.destroyed.connect(orb_died)
+
 
 func orb_died() -> void:
 	dead_orb_count += 1
